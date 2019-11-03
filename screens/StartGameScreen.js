11d7baeec891edb3f75e915e750f6b66fb5810f5
Abah <donny@nufaza.com>
2019-11-03 21:01:53 +0700

    Step 2 - Styling StartGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 const StartGameScreen = props => {
     return(
         <View style={styles.screen}>
-            <Text>The Game Screen!</Text>
+            <Text style={styles.title}>Start a New Game!</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a number!</Text>
+                <TextInput/>
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset" onPress={() => {}} />
+                    <Button title="Confirm" onPress={() => {}}/>
+                </View>
+            </View>
         </View>
     )
 };
@@ -14,6 +22,34 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        // Only for IOS
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        // Only for Android
+        elevation: 5,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    button: {
+        width: 80
     }
 });
 
