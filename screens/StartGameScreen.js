2b2803d45da7102a7d7434adf6bb2b122da3a44d
Abah <donny@nufaza.com>
2019-11-04 12:40:41 +0700

    Step 4 - Style buttons and reuse colors

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import Card from '../components/Card';
+import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
     return(
@@ -10,8 +11,12 @@ const StartGameScreen = props => {
                 <Text>Select a number!</Text>
                 <TextInput/>
                 <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={() => {}} />
-                    <Button title="Confirm" onPress={() => {}}/>
+                    <View style={styles.button}>
+                        <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Confirm" onPress={() => {}} color={Colors.primary}/>
+                    </View>
                 </View>
             </Card>
         </View>
@@ -40,7 +45,8 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15
     },
     button: {
-        width: 80
+        width: 100,
+        justifyContent:'center'
     }
 });
 
